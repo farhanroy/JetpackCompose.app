0b804017a61417db1a94d418092c6b72baaaace3
vinaygaba <vinaygaba@gmail.com>
2021-09-05 21:14:36 -0700

    Added template component for new pages

---
+++ src/components/newsletter/EarlyAccessPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../core/Layout";
+
+export default function EarlyAccessPage() {
+  return (
+    <>
+      <Layout maxWidth="md">
+        <div>Hello World this is great</div>
+      </Layout>
+    </>
+  );
+}
